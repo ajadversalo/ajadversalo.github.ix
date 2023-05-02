ea12e40f3191169465c3546d21eb32677c33aa74
Aj Adversalo <aj.adversalo@genxys.com>
2023-05-02 10:51:51 -0700

    'animate'

--- src/App.js
+++ src/App.js
@@ -5,13 +5,9 @@ function App() {
     return (
         <div className='App'>
             <div className="line1"></div>
-            <div style={{ width: '100%', textAlign: 'center', paddingTop: '15rem', color: '#FFF' }}>
-                <h1 style={{ margin: 0, fontSize: '4rem' }}><span style={{ color: 'transparent' }}>Hi, I'm</span> AJ<span style={{ color: 'transparent' }}>.</span></h1>
-                <h1>I'm a Full-Stack Software Developer.</h1>
-            </div>
-            <div style={{ textAlign: 'center' }}>
-                <p>About</p>
-                <p>Work</p>
+            <div className='parentX'  style={{ width: '100%', textAlign: 'center', paddingTop: '15rem' }} >
+                <h1 style={{ margin: 0, fontSize: '4rem' }}><span className='hi'>Hi, I'm</span> <span className='name'>AJ</span><span className='period'>.</span></h1>
+                <h1><span className='im'>I'm a</span> Full-Stack Software Developer.</h1>
             </div>
         </div>
     );
