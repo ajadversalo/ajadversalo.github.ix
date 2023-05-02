05f6169abbecc81609c23496a836e890ce5c8589
Aj Adversalo <aj.adversalo@genxys.com>
2023-05-02 11:53:07 -0700

    header update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ function App() {
     return (
         <div className='App'>
             <div className="line1"></div>
-            <div className='parentX'  style={{ width: '100%', textAlign: 'center', paddingTop: '15rem' }} >
-                <h1 style={{ margin: 0, fontSize: '4rem' }}><span className='hi'>Hi, I'm</span> <span className='name'>AJ</span><span className='period'>.</span></h1>
-                <h1><span className='im'>I'm a</span> Full-Stack Software Developer.</h1>
+            <div   style={{textAlign: 'center', paddingTop: '15rem' }} >
+                <div className='parentX'>
+                    <div style={{fontSize: '5rem' }}><span className='hi'>Hi, I'm</span> <span className='name'>AJ</span><span className='period'>.</span><span className='ln'>Adversalo</span></div>
+                    <div style={{ marginLeft: '70px', fontSize: '3rem' }}><span className='im'>I'm a</span> Full-Stack Software Developer.</div>
+                </div>
             </div>
         </div>
     );
