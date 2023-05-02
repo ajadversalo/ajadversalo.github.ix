a40707da16755522ed9afeba551312e4ae2f2304
Aj Adversalo <aj.adversalo@genxys.com>
2023-05-02 09:09:36 -0700

    test deploy

--- src/App.js
+++ src/App.js
@@ -2,24 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <div className="line1"></div>
+            <div style={{ width: '100%', textAlign: 'center', paddingTop: '15rem', color: '#FFF' }}>
+                <h1 style={{ margin: 0, fontSize: '4rem' }}><span style={{ color: 'transparent' }}>Hi, I'm</span> AJ<span style={{ color: 'transparent' }}>.</span></h1>
+                <h1>I'm a Full-Stack Software Developer.</h1>
+            </div>
+            <div style={{ textAlign: 'center' }}>
+                <p>About</p>
+                <p>Work</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
