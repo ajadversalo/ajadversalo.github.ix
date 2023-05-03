256c94e6f2a25210ef45b38b94645d40de36a123
Aj Adversalo <aj.adversalo@genxys.com>
2023-05-03 08:52:53 -0700

    layout fixes

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ const useStyles = makeStyles()(() => ({
     },
     headingRoot: {
         textAlign: 'center',
-        //paddingTop: '15rem',
         color: '#FFF',
         maxWidth: '40rem',
         minWidth: '15rem',
@@ -80,7 +79,7 @@ const useStyles = makeStyles()(() => ({
         color: '#FFF',
         display: 'flex',
         flexDirection: 'row',
-        paddingTop: '7rem',
+        paddingTop: '4rem',
         width: '100%',
         justifyContent: 'space-around'
     }
@@ -106,7 +105,7 @@ function App() {
             </div>
             <div className="line delay" style={{left: '98%'}}></div>
             <div style={{height: '100%'}}>
-                <div className={classes.headingRoot}>                  
+                <div className={classes.headingRoot} style={{paddingTop: isSm ? '9rem' : '13rem'}}>                  
                     <div style={{ fontSize: 'calc(0.5rem + 1vw)', color: '#CCDBDC', fontWeight: 600}}>Hi There! I'm</div>
                     <div style={{ fontSize: 'calc(2rem + 2vw)', padding: '1rem 0' }}>AJ Adversalo</div>                                     
                     <div style={{ fontSize: 'calc(1rem + 1vw)', color: '#CCDBDC' }}>I'm a full-stack software developer passionate in creating robust, user-friendly web applications that make a difference.</div>                         
